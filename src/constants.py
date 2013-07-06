a3ea763ee8fb476c485684af1cf1de0cf81aa947
Daniel Bateman <jetison.24@gmail.com>
2013-07-06 04:07:15 -0500

    Bump to 0.6.3

--- src/constants.py
+++ src/constants.py
@@ -16,8 +16,8 @@
 
 import wx, os, sys
 
-VERSION = "0.6.2"
-VERSION_INT = 11
+VERSION = "0.6.3"
+VERSION_INT = 14
 URL_CHECK = "http://faultexception.com/swap/check_updates.php"
 PARSER_SERVER_ADDR = ("swapserver.no-ip.biz", 57680)
 IS_COMPILED = 'frozen' in dir(sys)
