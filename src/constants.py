a27760e6c2bd5c8c3f766b94be551f297752969a
Daniel Bateman <jetison.24@gmail.com>
2013-07-19 22:31:22 -0500

    Update version

--- src/constants.py
+++ src/constants.py
@@ -16,8 +16,8 @@
 
 import wx, os, sys
 
-VERSION = "0.7.7"
-VERSION_INT = 22
+VERSION = "0.7.7a"
+VERSION_INT = 23
 URL_CHECK = "http://faultexception.com/swap/check_updates.php"
 LOG_SEND_URL = "http://faultexception.com/swap/sendlog.php"
 PARSER_SERVER_ADDR = ("swapserver.no-ip.biz", 57680)
