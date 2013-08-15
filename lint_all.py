ef284ad050b4e99ae977b6179b2591c4467444db
Daniel Bateman <jetison.24@gmail.com>
2013-08-15 08:55:05 -0500

    Added lint_all.py

---
+++ lint_all.py
@@ -0,0 +1,20 @@
+import os, sys
+
+extra = ['external', 'pylint_stubs']
+extra = [os.getcwd() + "/" + e for e in extra]
+initHook = "import sys; sys.path = sys.path + " + repr(extra)
+
+pylintCmd = "pylint %%s -E --rcfile=%s --init-hook=\"%s\""%(os.getcwd() + "/pylint.rc", initHook)
+
+def ret_or_die(x):
+    if x:
+        exit(x)
+
+ret_or_die(os.system(pylintCmd%"build.py clean.py run.py package.py lint_all.py"))
+ret_or_die(os.chdir("external"))
+ret_or_die(os.system(pylintCmd%"fuzion"))
+ret_or_die(os.chdir("../src"))
+ret_or_die(os.system(pylintCmd%(" ".join([x for x in os.listdir(".") if x.endswith('.py')]))))
+ret_or_die(os.chdir(".."))
+
+exit(0)
