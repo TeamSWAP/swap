c170de8bdb1b64238402ee6f0a4549655009399d
Daniel Bateman <jetison.24@gmail.com>
2013-09-14 04:12:57 -0500

    Add Operative/Shadow/Assassin disappear abilities

--- src/log_parser.py
+++ src/log_parser.py
@@ -31,6 +31,12 @@ from logging import prnt
 # [18:36:21.225] [Cartel Patrol Droid {2981965728841728}:3535188148330] [@Bellestarr] [Explosive Round {827176341471232}] [ApplyEffect {836045448945477}: Damage {836045448945501}] (1216 kinetic {836045448940873}) <1216>
 
 DISAPPEAR_GRACE = 15
+DISAPPEAR_ABILITIES = (
+	'2276212407795712', # Scoundrel
+	'2278037768896512', # Operative
+	'2271612497821696', # Shadow
+	'2271329029980160', # Assassin
+)
 
 # Global variables
 parserThread = None
@@ -259,7 +265,7 @@ class Parser(events.EventSource):
 						event.exitEvent = True
 
 					# Detect disappear
-					if self.fights and event.type == GameEvent.TYPE_ABILITY_ACTIVATE and event.ability == '2276212407795712':
+					if self.fights and event.type == GameEvent.TYPE_ABILITY_ACTIVATE and event.ability in DISAPPEAR_ABILITIES:
 						lastFight = self.fights[-1]
 						exitBluff = False
 						# Look back for exit combat.
